bd1e041982b2d4ee7f9ab0ff38aeba9ce85362f8
pony <165937828+engineering-wrench@users.noreply.github.com>
2026-03-13 20:54:57 +0300

    new update

--- Site/backend/js/interface.js
+++ Site/backend/js/interface.js
@@ -3,11 +3,18 @@ import { splitImage } from '/backend/js/utilit.js';
 const simulation_swicth = document.getElementById('switch')
 const status_text = document.getElementById('status')
 const el_container = document.getElementById('el_container')
+const menu_option = document.getElementById('menu-option')
+const back_button = document.getElementById('back_button')
+const category_menu = document.getElementById('category-menu')
 
-const idList = [
+const idListCategoryEl = [
     '???', 'sources', 'component', 'diod', 
     'transistor', 'analog', 'mixed', 'digit',
     'logic', '???', '???', 'function?', 'text', 'instrument', '???', '???'
+];
+
+const idListMenuOption = [
+    'new', 'open', 'save', 'print'
 ];
 
 const state_list = [
@@ -37,11 +44,30 @@ async function display() {
             div.style.backgroundImage = `url(${tile.dataUrl})`;
             
             // Добавляем ID из списка (если есть)
-            if (idList[index]) {
-                div.id = idList[index];
+            if (idListCategoryEl[index]) {
+                div.id = idListCategoryEl[index];
             }
             
             el_container.appendChild(div);
+        });
+    } catch (error) {
+        console.error('Ошибка при разрезании картинки:', error);
+        state = state_list[3]; // error
+    }
+        try {
+        const result = await splitImage('/img/title/menu_option.bmp', 24);
+        
+        result.tiles.forEach((tile, index) => {
+            
+            const div = document.createElement('div');
+            div.style.backgroundImage = `url(${tile.dataUrl})`;
+            
+            // Добавляем ID из списка (если есть)
+            if (idListMenuOption[index]) {
+                div.id = idListMenuOption[index];
+            }
+            
+            menu_option.appendChild(div);
         });
     } catch (error) {
         console.error('Ошибка при разрезании картинки:', error);
@@ -68,11 +94,38 @@ function switch_click() {
     }
 }
 
-function main () {
-    display ()
+function back_to_main() {
+    window.location.href = '/index.html'
+}
+
+async function main () {
+    await display ()
 }
 
 main ()
 
 
 simulation_swicth.addEventListener('click', switch_click)
+back_button.addEventListener('click', back_to_main)
+
+function open_catalog (target) {
+    if (target.id === 'instrument') {
+        category_menu.style.display = 'block'
+
+        const rect = target.getBoundingClientRect();
+        
+        category_menu.style.left = rect.left - 100 + 'px';
+        category_menu.style.top = (rect.bottom + 25) + 'px';
+    }
+}
+
+document.addEventListener('click', function () {
+    if (!(event.target.parentElement.id === 'el_container'))
+    category_menu.style.display = 'none'
+})
+
+el_container.addEventListener('click', function(event) {
+    const target = event.target;
+    open_catalog(target);
+});
+
